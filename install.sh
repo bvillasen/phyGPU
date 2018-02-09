1e3c3493a3a639ff0fdaf5db0c042b88feb320e8
bvillasen <bvillasen@gmail.com>
2018-02-09 15:44:55 -0800

    added tools

--- install.sh
+++ install.sh
@@ -21,43 +21,43 @@ LINE="source $ENV_DIR/$ENV_NAME/bin/activate"
 echo $LINE >> $BIN_DIR/activate_phyGPU
 chmod 777 $BIN_DIR/activate_phyGPU
 #
-# #Add binaries directory to your path
-# LINE="export PATH=$BIN_DIR:\$PATH"
-# LAST_LINE=$(awk '/./{line=$0} END{print line}' ~/.bashrc)
-# if [ "$LAST_LINE" != "$LINE" ]; then
-#   echo "Adding binaries directory to .bashrc file"
-#   echo $LINE >> ~/.bashrc
-# fi
+#Add binaries directory to your path
+LINE="export PATH=$BIN_DIR:\$PATH"
+LAST_LINE=$(awk '/./{line=$0} END{print line}' ~/.bashrc)
+if [ "$LAST_LINE" != "$LINE" ]; then
+  echo "Adding binaries directory to .bashrc file"
+  echo $LINE >> ~/.bashrc
+fi
 #
-# # Activate the pycuda enviroment and install packages
-# . ./$ENV_NAME/bin/activate
-# pip install ipython[all]
-# pip install numpy
-# pip install matplotlib
-# pip install PyOpenGL
-#
-# #########################################################
-# #INSTALL PYCUDA
-# wget https://pypi.python.org/packages/source/p/pycuda/pycuda-2015.1.3.tar.gz
-# tar xzvf pycuda-2015.1.3.tar.gz -C $ENV_DIR/$ENV_NAME/
-# rm pycuda-2015.1.3.tar.gz
-# cd $ENV_DIR/$ENV_NAME/pycuda-2015.1.3
-# ./configure.py
-# #Replace line in siteconf.py to enable graphics
-# #Create temporary file with new line in place
-# cat siteconf.py | sed -e "s/CUDA_ENABLE_GL = False/CUDA_ENABLE_GL = True/" > siteconf_temp.py
-# #Copy the new file over the original file
-# mv siteconf_temp.py siteconf.py
-# make -j 4
-# python setup.py install
-# pip install .
-# cd $ENV_DIR/$ENV_NAME
-# #########################################################
-#
-# #Download projects to projects directory
-# mkdir projects
-# cd projects
-# git clone https://github.com/bvillasen/tools.git
-# git clone https://github.com/bvillasen/volumeRender.git
-# git clone https://github.com/bvillasen/animation2D.git
-# git clone https://github.com/bvillasen/isingModel.git
+# Activate the pycuda enviroment and install packages
+. ./$ENV_NAME/bin/activate
+pip install ipython[all]
+pip install numpy
+pip install matplotlib
+pip install PyOpenGL
+
+#########################################################
+#INSTALL PYCUDA
+wget https://pypi.python.org/packages/source/p/pycuda/pycuda-2015.1.3.tar.gz
+tar xzvf pycuda-2015.1.3.tar.gz -C $ENV_DIR/$ENV_NAME/
+rm pycuda-2015.1.3.tar.gz
+cd $ENV_DIR/$ENV_NAME/pycuda-2015.1.3
+./configure.py
+#Replace line in siteconf.py to enable graphics
+#Create temporary file with new line in place
+cat siteconf.py | sed -e "s/CUDA_ENABLE_GL = False/CUDA_ENABLE_GL = True/" > siteconf_temp.py
+#Copy the new file over the original file
+mv siteconf_temp.py siteconf.py
+make -j 4
+python setup.py install
+pip install .
+cd $ENV_DIR/$ENV_NAME
+#########################################################
+
+#Download projects to projects directory
+mkdir projects
+cd projects
+git clone https://github.com/bvillasen/tools.git
+git clone https://github.com/bvillasen/volumeRender.git
+git clone https://github.com/bvillasen/animation2D.git
+git clone https://github.com/bvillasen/isingModel.git
